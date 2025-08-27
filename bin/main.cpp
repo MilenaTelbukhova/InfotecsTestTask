03a99a1ff2a7213c1a03e0dd3e8596a4b6dd6016
CapToff <tmilenatt@yandex.ru>
2025-08-27 20:42:20 +0300

    unwanted files and logic deleted

--- bin/main.cpp
+++ bin/main.cpp
@@ -10,37 +10,24 @@
 
 int main(int argc, char*argv[])
 {
-    if (argc < 4) {
+    if (argc < 3) {
         std::cout << "Not enought arguments" << std::endl;
         return 0;
     }
     
     std::string importanceLevelstr = std::string(argv[1]) + " ";
     ImportanceLevel importanceLevel = MessageParse::ParseCommand(ImportanceLevel::debug, importanceLevelstr).first;
-    std::string flag = argv[2];
-    if (flag.size() != 2 && flag[0] != '-'){
-        std::cout << "invalid flag input" << std::endl;
-        return 0;
-    }
-
-    if (flag[1] == 'f') {
-        std::filesystem::path path = argv[3];
-        Logger logger{path, importanceLevel};
-        while (true)
-        {
-            std::string s;
-            std::getline(std::cin, s);
-            auto res = MessageParse::ParseCommand(logger.GetImportanceLevel(), s);
-            std::thread th ([&logger](ImportanceLevel importance, const std::string& message){
-                auto err = logger.Log(importance, message);
-                if (err.has_value()) std::cout << err.value().Message;
-            }, res.first, res.second);
-            th.detach();
-        }
-    } else if (flag[1] == 's') {
-        //socket logic
-    } else {
-        std::cout << "invalid flag input" << std::endl;
+    std::filesystem::path path = argv[2];
+    Logger logger{path, importanceLevel};
+    while (true) {
+        std::string s;
+        std::getline(std::cin, s);
+        auto res = MessageParse::ParseCommand(logger.GetImportanceLevel(), s);
+        std::thread th ([&logger](ImportanceLevel importance, const std::string& message){
+            auto err = logger.Log(importance, message);
+            if (err.has_value()) std::cout << err.value().Message;
+        }, res.first, res.second);
+        th.detach();
     }
 
     return 0;
